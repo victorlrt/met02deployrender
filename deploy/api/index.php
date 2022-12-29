bee42faadc11aa9bf74c2aafd94787d89596e8ce
Victor <vlarmet@cfai-formation.fr>
2022-12-29 16:40:05 +0100

    wip

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -66,45 +66,37 @@ function  addHeaders (Response $response) : Response {
 
 
 //APi d'authentification générant un JWT
-$app->post('/api/login', function (Request $request, Response $response, $args) {   
-    $err=false;
-    $inputJSON = file_get_contents('php://input');
-    $body = json_decode( $inputJSON, TRUE ); //convert JSON into array
+// $app->post('/api/login', function (Request $request, Response $response, $args) {   
+//     $err=false;
+//     $inputJSON = file_get_contents('php://input');
+//     $body = json_decode( $inputJSON, TRUE ); //convert JSON into array
 
-    $login = $body ['login'] ?? "";
-    $password = $body ['password'] ?? "";
+//     $login = $body ['login'] ?? "";
+//     $password = $body ['password'] ?? "";
 
-    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
-        $err = true;
-    }
-    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$password))  {
-        $err=true;
-    }
+//     if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
+//         $err = true;
+//     }
+//     if (!preg_match("/[a-zA-Z0-9]{1,20}/",$password))  {
+//         $err=true;
+//     }
 
-    global $entityManager;
-    $user = $entityManager->getRepository('Client')->findOneBy(array('login' => $login, 'password' => $password));
-
-
-    if (!$err && $user) {
-            $response = createJwT ($response, $login, $password);
-            $response = addHeaders($response);
-            $data = array('login' => $login);
-            $response->getBody()->write(json_encode($data));
-     } else {          
-            $response = $response->withStatus(401);
-     }
-    return $response;
-});
+//     global $entityManager;
+//     $user = $entityManager->getRepository('Client')->findOneBy(array('login' => $login, 'password' => $password));
 
 
-
-// $app->get('/api/catalogue', function (Request $request, Response $response, $args) {
-//     $json = file_get_contents("../assets/db/dbMushroom.json");
-//     $array = json_decode($json, true);
-//     $response = addHeaders($response);
-//     $response->getBody()->write($json);
+//     if (!$err && $user) {
+//             $response = createJwT ($response, $login, $password);
+//             $response = addHeaders($response);
+//             $data = array('login' => $login);
+//             $response->getBody()->write(json_encode($data));
+//      } else {          
+//             $response = $response->withStatus(401);
+//      }
 //     return $response;
 // });
+
+
 
 $app->get('/api/catalogue', function (Request $request, Response $response, $args) {
     global $entityManager;
@@ -116,11 +108,11 @@ $app->get('/api/catalogue', function (Request $request, Response $response, $arg
 
 
 $app->add(new Tuupola\Middleware\JwtAuthentication($options));
-// $app->add(new Tuupola\Middleware\CorsMiddleware([
-//     "origin" => ["*"],
-//     "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
-//     "headers.allow" => ["Authorization", "Content-Type"],
-//     "headers.expose" => ["Authorization"],
-//     "headers.origin" => ["*"],
-// ]));
+$app->add(new Tuupola\Middleware\CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => ["Authorization", "Content-Type"],
+    "headers.expose" => ["Authorization"],
+    "headers.origin" => ["*"],
+]));
 $app->run ();
