e2cdb568a639c90a3f9d1db869789f41efa1d75a
Victor <vlarmet@cfai-formation.fr>
2022-12-30 17:25:16 +0100

    wip

--- deploy/api/model.php
+++ deploy/api/model.php
@@ -6,64 +6,13 @@ class Client
     public $id;
     public $firstname;
     public $lastname;
-    public $login;
-    public $civility;
-    public $phone;
-    public $email;
-    public $password;
-    public $address;
-    public $city;
     public $zipCode;
-    public $country;
+    public $tel;
+    public $email;
+    public $gender;
+    public $login;
+    public $password;
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-    public function setFirstname($firstname)
-    {
-        $this->firstname = $firstname;
-    }
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-    }
-    public function setLogin($login)
-    {
-        $this->login = $login;
-    }
-    public function setCivility($civility)
-    {
-        $this->civility = $civility;
-    }
-    public function setPhone($phone)
-    {
-        $this->phone = $phone;
-    }
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-    public function setAddress($address)
-    {
-        $this->address = $address;
-    }
-    public function setCity($city)
-    {
-        $this->city = $city;
-    }
-    public function setzipCode($zipCode)
-    {
-        $this->zipCode = $zipCode;
-    }
-    public function setCountry($country)
-    {
-        $this->country = $country;
-    }
 }
 
 class Mushroom {
