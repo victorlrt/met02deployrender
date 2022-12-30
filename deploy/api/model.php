3ef19835e84456f2c9a7654c8e80d7bf9999f7d3
Victor <vlarmet@cfai-formation.fr>
2022-12-30 19:04:19 +0100

    wip (work before)

--- deploy/api/model.php
+++ deploy/api/model.php
@@ -1,56 +1,56 @@
 <?php
 
 //class client
-class Client
-{
-    public $id;
-    public $firstname;
-    public $lastname;
-    public $zipcode;
-    public $tel;
-    public $email;
-    public $gender;
-    public $login;
-    public $password;
+// class Client
+// {
+//     public $id;
+//     public $firstname;
+//     public $lastname;
+//     public $zipcode;
+//     public $tel;
+//     public $email;
+//     public $gender;
+//     public $login;
+//     public $password;
 
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-    public function setFirstname($firstname)
-    {
-        $this->firstname = $firstname;
-    }
-    public function setLastname($lastname)
-    {
-        $this->lastname = $lastname;
-    }
-    public function setZipCode($zipcode)
-    {
-        $this->zipcode= $zipcode;
-    }
-    public function setTel($tel)
-    {
-        $this->tel = $tel;
-    }
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-    public function setGender($gender)
-    {
-        $this->gender = $gender;
-    }
-    public function setLogin($login)
-    {
-        $this->login = $login;
-    }
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
+//     public function setId($id)
+//     {
+//         $this->id = $id;
+//     }
+//     public function setFirstname($firstname)
+//     {
+//         $this->firstname = $firstname;
+//     }
+//     public function setLastname($lastname)
+//     {
+//         $this->lastname = $lastname;
+//     }
+//     public function setZipCode($zipcode)
+//     {
+//         $this->zipcode= $zipcode;
+//     }
+//     public function setTel($tel)
+//     {
+//         $this->tel = $tel;
+//     }
+//     public function setEmail($email)
+//     {
+//         $this->email = $email;
+//     }
+//     public function setGender($gender)
+//     {
+//         $this->gender = $gender;
+//     }
+//     public function setLogin($login)
+//     {
+//         $this->login = $login;
+//     }
+//     public function setPassword($password)
+//     {
+//         $this->password = $password;
+//     }
 
-}
+// }
 
 class Mushroom {
     public $id;
