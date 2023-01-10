525a3d59c3adff1aa0672932848f997602efcd47
Victor <vlarmet@cfai-formation.fr>
2023-01-10 12:52:00 +0100

    wip

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -6,11 +6,11 @@ require_once "vendor/autoload.php";
 $isDevMode = true;
 $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
 $conn = array(
-'host' => 'dpg-ceu24j02i3mj6pk3efsg-a',
+'host' => 'dpg-ceuk0hg2i3ml690jjabg-a',
 'driver' => 'pdo_pgsql',
-'user' => 'met02db_user',
-'password' => 'zO3y3HLIOqL0Edk6NkqGp9UbX7CAYL3T',
-'dbname' => 'met02db',
+'user' => 'met02_db_user',
+'password' => 'ETMjsEaNYe2qiQOm0QNJyECxTrj3uWUs',
+'dbname' => 'met02_db',
 'port' => '5432'
 );
 $entityManager = EntityManager::create($conn, $config);
